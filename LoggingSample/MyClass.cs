52ca952bf7abec21521058d93bf572160f0b1ea2
JakeLardinois <jake.lardinois@gmail.com>
2016-11-07 05:53:54 -0600

    Added Logging to a Class

---
+++ LoggingSample/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logging;
+
+namespace LoggingSample
+{
+    public class MyClass
+    {
+        private static readonly ILog Logger = LogManager.GetInstance<MyClass>();
+
+        public MyClass()
+        {
+            Logger.Debug("Logging in my Class");
+        }
+    }
+}
