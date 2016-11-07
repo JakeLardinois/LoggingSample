9c159bb82d291306cbb485f40e8ac03ba554cb24
JakeLardinois <jake.lardinois@gmail.com>
2016-11-07 05:53:02 -0600

    Implemented and Configured Logging to Project

--- LoggingSample/Program.cs
+++ LoggingSample/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logging;
 
 namespace LoggingSample
 {
     class Program
     {
+        private static readonly ILog Logger = LogManager.GetInstance<Program>();
+        private static readonly string ServicePath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).FullName;
+
+
         static void Main(string[] args)
         {
+            ConfigureLogging();
+        }
+
+        private static void ConfigureLogging()
+        {
+            //Could add some more properties to log here if you want to...
+            IDictionary<string, object> loggingProperties = new Dictionary<string, object>();
+
+            var mappedPath = ServicePath + "app.config";
+            LogManager.Configure(mappedPath, loggingProperties);
+
+            Logger.Info("Logging Started...");
         }
     }
 }
