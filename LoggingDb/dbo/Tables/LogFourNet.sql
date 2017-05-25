cf62dcee2362d2f36782acf697b5bde8f99849ce
JakeLardinois <jake.lardinois@gmail.com>
2017-05-25 11:40:49 -0500

    Added Alternative Logging Table

---
+++ LoggingDb/dbo/Tables/LogFourNet.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[LogFourNet] (
+    [ID]        BIGINT         IDENTITY (1, 1) NOT NULL,
+    [DateStamp] DATETIME       NOT NULL,
+    [Thread]    VARCHAR (255)  NOT NULL,
+    [ErrLevel]  VARCHAR (20)   NOT NULL,
+    [Logger]    VARCHAR (255)  NOT NULL,
+    [Message]   VARCHAR (4000) NOT NULL,
+    [AppName]   VARCHAR (50)   NULL,
+    [Exception] VARCHAR (8000) NULL,
+    CONSTRAINT [PK_LogFourNet] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
